46556fb21d67d37b2f00ef98187748601ed09265
Heil Park <heilpark@me.com>
2023-01-20 18:44:33 +0000

    js required files commit

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+var questions = [
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: ""
+    },
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: ""
+    },
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: ""
+    },
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        choice4: ""
+    }
+]
