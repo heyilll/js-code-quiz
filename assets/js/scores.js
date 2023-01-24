79cde2952b78c2df04e227670a3a3b0d966f60fe
Heil Park <heilpark@me.com>
2023-01-24 22:24:20 +0000

    highscores sorted numerically

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,7 @@
 var highscores = document.querySelector("#highscores");
 var clearButton = document.querySelector("#clear");
+// stores the scores in descending order
+var sortedArr = [];
 
 //renders scores
 renderScore();
@@ -8,16 +10,28 @@ renderScore();
 function renderScore() {
     if (localStorage.length == 0) {
         highscores.innerHTML ="";
+        return;
     }
 
+    sortScores();
+    for (let i = 0; i < localStorage.length; i++) {
+        var initials = sortedArr[i][0];
+        var score = sortedArr[i][1];
+
+        var li = document.createElement("li");
+        li.textContent = initials + ' = ' + score;
+        highscores.appendChild(li);
+    }
+}
+
+// sorts an array of stored scores in descending order
+function sortScores() {
     for (let i = 0; i < localStorage.length; i++) {
         var key = localStorage.key(i);
         var score = localStorage.getItem(key);
-
-        var li = document.createElement("li");
-        li.textContent = key + ' = ' + score;
-        highscores.appendChild(li);
+        sortedArr[i] = [key, score];
     }
+    sortedArr.sort(function(a, b){return b[1] - a[1]});
 }
 
 // clears all data in local storage and re renders the page
